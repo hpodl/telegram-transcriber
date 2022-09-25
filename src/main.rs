25e02a544926801892193326f26cffc8b79ee6d0
hpodl <hubert.podlewski.stud@pw.edu.pl>
2022-09-25 21:38:31 +0200

    First 'fully'  working prototype

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,13 @@
+use std::process::Command;
+use std::str;
 use teloxide::{
     net::Download,
     prelude::*,
-    types::{File as TelegramFile, MediaKind, MessageKind},
+    types::{self, File as TelegramFile, MediaKind, MessageKind},
 };
 use tokio::fs::File;
 
-const MAX_DUR: u32 = 60;
+const MAX_DUR: u32 = 75;
 
 #[tokio::main]
 async fn main() {
@@ -14,23 +16,64 @@ async fn main() {
     let bot = Bot::from_env().auto_send();
 
     teloxide::repl(bot, |message: Message, bot: AutoSend<Bot>| async move {
-        if let MessageKind::Common(kind) = message.kind {
-            if let MediaKind::Voice(voice_kind) = kind.media_kind {
-                let voice = voice_kind.voice;
-                if voice.duration < MAX_DUR {
-                    let TelegramFile { meta, file_path } =
-                        bot.get_file(voice.file_id).send().await?;
-                    let mut local_file = File::create(meta.file_unique_id.to_string()).await?;
+        if let Some(voice) = get_voice_from_message(&message) {
+            if voice.duration < MAX_DUR {
+                let TelegramFile { meta, file_path } = bot.get_file(&voice.file_id).send().await?;
+                let local_filename = meta.file_unique_id.as_str();
+                let mut local_file = File::create(local_filename).await?;
 
-                    println!("Downloading voice file...");
-                    bot.download_file(&file_path, &mut local_file).await?;
-                    println!("Finished downloading voice file...");
-                }
+                println!("Downloading voice file...");
+                bot.download_file(&file_path, &mut local_file).await?;
+                println!("Finished downloading voice file...");
+
+                bot.send_message(
+                    message.chat.id,
+                    transcribe(local_filename)
+                        .await
+                        .unwrap_or("Failed to transcribe the message".to_string()),
+                )
+                .await?;
             }
         }
 
-        bot.send_dice(message.chat.id).await?;
         respond(())
     })
     .await;
+}
+
+#[inline]
+fn get_voice_from_message(message: &Message) -> Option<&types::Voice> {
+    if let MessageKind::Common(kind) = &message.kind {
+        if let MediaKind::Voice(voice_kind) = &kind.media_kind {
+            return Some(&voice_kind.voice);
+        }
+    }
+    None
+}
+
+async fn transcribe(path: &str) -> Result<String, ()> {
+    if cfg!(target_os = "windows") {
+        panic!()
+    }
+
+    eprintln!("Starting transcription of {}.", path);
+    let output = Command::new("sh")
+        .arg("-c")
+        .arg(format!(
+            "whisper --model small --language pl --task transcribe {path}"
+        ))
+        .output();
+
+    match output {
+        Ok(result) => {
+            let transcribed = str::from_utf8(&result.stdout).unwrap().to_string();
+            eprintln!("{}", str::from_utf8(&result.stderr).unwrap());
+
+            Ok(transcribed)
+        }
+        Err(e) => {
+            eprintln!("Failed transcribing {}: {}", path, e);
+            Err(())
+        }
+    }
 }
