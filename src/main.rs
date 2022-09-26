cdfe87c0fecb2db1d71007be80f75bdbda22fc2e
hpodl <hubert.podlewski.stud@pw.edu.pl>
2022-09-26 11:38:49 +0200

    Added prototype command handling

--- src/main.rs
+++ src/main.rs
@@ -1,88 +1,57 @@
-use std::process::Command;
-use std::str;
-use teloxide::{
-    net::Download,
-    prelude::*,
-    types::{self, File as TelegramFile, MediaKind, MessageKind},
-};
-use tokio::fs::File;
+use teloxide::{prelude::*, utils::command::BotCommands};
 
-const MAX_DUR: u32 = 75;
+use std::error::Error;
 
 #[tokio::main]
 async fn main() {
-    println!("Starting up the bot...");
-
     let bot = Bot::from_env().auto_send();
 
-    teloxide::repl(bot, |message: Message, bot: AutoSend<Bot>| async move {
-        if let Some(voice) = get_voice_from_message(&message) {
-            if voice.duration < MAX_DUR {
-                let TelegramFile { meta, file_path } = bot.get_file(&voice.file_id).send().await?;
-                let local_filename = meta.file_unique_id.as_str();
-                let mut local_file = File::create(local_filename).await?;
+    teloxide::commands_repl(bot, answer, Command::ty()).await;
+}
 
-                println!("Downloading voice file...");
-                bot.download_file(&file_path, &mut local_file).await?;
-                println!("Finished downloading voice file...");
+#[derive(BotCommands, Clone)]
+#[command(rename = "lowercase", description = "These commands are supported:")]
+enum Command {
+    #[command(description = "display this text.")]
+    Help,
+    #[command(description = "ping.")]
+    Ping,
+    #[command(description = "transcribe voice message being replied to")]
+    Transcribe,
+}
 
-                bot.send_message(
-                    message.chat.id,
-                    transcribe(local_filename)
-                        .await
-                        .unwrap_or("Failed to transcribe the message".to_string()),
-                )
-                .reply_to_message_id(message.id)
-                .await?;
-            }
-        } else if Some("ping") == message.text() {
-            bot.send_message(
-                message.chat.id,
-                format!("Pong"),
-            )
-            .reply_to_message_id(message.id)
-            .await
-            .unwrap();
+async fn answer(
+    bot: AutoSend<Bot>,
+    message: Message,
+    command: Command,
+) -> Result<(), Box<dyn Error + Send + Sync>> {
+    match command {
+        Command::Help => {
+            bot.send_message(message.chat.id, Command::descriptions().to_string())
+                .await?
         }
+        Command::Ping => bot.send_message(message.chat.id, "Pong.").await?,
+        Command::Transcribe => transcribe_handler(&bot, &message).await?,
+    };
 
-        respond(())
-    })
-    .await;
+    Ok(())
 }
 
 #[inline]
-fn get_voice_from_message(message: &Message) -> Option<&types::Voice> {
-    if let MessageKind::Common(kind) = &message.kind {
-        if let MediaKind::Voice(voice_kind) = &kind.media_kind {
-            return Some(&voice_kind.voice);
+async fn transcribe_handler(
+    bot: &AutoSend<Bot>,
+    message: &Message,
+) -> Result<Message, teloxide::RequestError> {
+    Ok(match message.reply_to_message() {
+        Some(replied) => {
+            bot.send_message(message.chat.id, "Hello o/")
+                .reply_to_message_id(replied.id)
+                .await?
         }
-    }
-    None
-}
-
-async fn transcribe(path: &str) -> Result<String, ()> {
-    if cfg!(target_os = "windows") {
-        panic!()
-    }
-
-    eprintln!("Starting transcription of {}.", path);
-    let output = Command::new("sh")
-        .arg("-c")
-        .arg(format!(
-            "whisper --model small --language pl --task transcribe {path}"
-        ))
-        .output();
-
-    match output {
-        Ok(result) => {
-            let transcribed = str::from_utf8(&result.stdout).unwrap().to_string();
-            eprintln!("{}", str::from_utf8(&result.stderr).unwrap());
-
-            Ok(transcribed)
+        None => {
+            bot.send_message(message.chat.id, "No message is being replied to.")
+                .reply_to_message_id(message.id)
+                .await?
         }
-        Err(e) => {
-            eprintln!("Failed transcribing {}: {}", path, e);
-            Err(())
-        }
-    }
+    })
 }
