993bf03cdc28b7f5c8abd0997b0e402972842125
hpodl <hubert.podlewski.stud@pw.edu.pl>
2022-09-28 21:33:21 +0200

    Transcribe now returns a result; added ability to choose transcription model

--- src/main.rs
+++ src/main.rs
@@ -21,7 +21,13 @@ enum Command {
     #[command(description = "ping.")]
     Ping,
     #[command(description = "transcribe voice message being replied to.")]
-    Transcribe,
+    // string parameter specifies which model to use
+    Transcribe(String),
+    #[command(
+        description = "placeholder for testing functionality.",
+        parse_with = "split"
+    )]
+    Test(String),
 }
 
 async fn answer(
@@ -35,7 +41,8 @@ async fn answer(
                 .await?
         }
         Command::Ping => bot.send_message(message.chat.id, "Pong.").await?,
-        Command::Transcribe => transcribe_handler(&bot, &message).await?,
+        Command::Transcribe(model) => transcribe_handler(&bot, &message, &model).await?,
+        Command::Test(param) => test_handler(&bot, &message, &param).await?,
     };
 
     Ok(())
@@ -47,22 +54,41 @@ async fn answer(
 async fn transcribe_handler(
     bot: &AutoSend<Bot>,
     message: &Message,
+    model: &str,
 ) -> Result<Message, teloxide::RequestError> {
+    let model = if !is_valid_model(model) {
+        if !model.is_empty() {
+            bot.send_message(message.chat.id, "Invalid model specified, using 'small'")
+                .reply_to_message_id(message.id)
+                .await?;
+        }
+        "small"
+    } else {
+        model
+    };
+
     Ok(match message.reply_to_message() {
-        Some(replied) => {
-            if let Some(transcribed) = transcribe_or_none(bot, replied).await {
+        Some(replied) => match try_transcribe(bot, replied, model).await {
+            Ok(transcribed) => {
                 bot.send_message(message.chat.id, transcribed)
                     .reply_to_message_id(replied.id)
                     .await?
-            } else {
-                bot.send_message(message.chat.id, "Not a voice message")
-                    .await?
             }
-        }
+            Err(e) => bot.send_message(message.chat.id, e).await?,
+        },
         None => {
             bot.send_message(message.chat.id, "No message is being replied to.")
                 .reply_to_message_id(message.id)
                 .await?
         }
     })
+}
+
+async fn test_handler(
+    bot: &AutoSend<Bot>,
+    message: &Message,
+    param: &str,
+) -> Result<Message, teloxide::RequestError> {
+    bot.send_message(message.chat.id, format!("Given: {}", param))
+        .await
 }
--- src/transcribe.rs
+++ src/transcribe.rs
@@ -1,14 +1,36 @@
+use std::collections::HashSet;
 use std::process::Command as OsCommand;
 use teloxide::types::File as TelegramFile;
 use teloxide::{net::Download, prelude::*};
 
 use tokio::fs::{self, File};
 
-/// Returns `Some(transcription)` of audio if `message` contains any
+use lazy_static::lazy_static;
+
+lazy_static! {
+    static ref AVAILABLE_MODELS: HashSet<&'static str> = {
+        let mut set = HashSet::new();
+        set.insert("tiny");
+        set.insert("base");
+        set.insert("small");
+        set.insert("medium");
+        set.insert("large");
+        set
+    };
+}
+
+// TODO Refector out the need to pass model parameter through multiple function calls
+// TODO Refactor out the workaround of using whisper as a shell command
+
+/// Returns `Ok(transcription)` of audio if `message` contains any
 /// even if the transcription happens to be an empty string
 ///
-/// Returns `None` if it doesn't
-pub async fn transcribe_or_none(bot: &AutoSend<Bot>, message: &Message) -> Option<String> {
+/// Else returns `Err(error_messsage)`
+pub async fn try_transcribe(
+    bot: &AutoSend<Bot>,
+    message: &Message,
+    model: &str,
+) -> Result<String, String> {
     let file_id = {
         if let Some(media) = message.voice() {
             &media.file_id
@@ -19,27 +41,30 @@ pub async fn transcribe_or_none(bot: &AutoSend<Bot>, message: &Message) -> Optio
         } else if let Some(media) = message.video_note() {
             &media.file_id
         } else {
-            return None;
+            return Err("Doesn't contain audio media.".to_string());
         }
     };
 
-    download_and_transcribe(bot, file_id).await.ok()
+    download_and_transcribe(bot, file_id, model).await
 }
 
-// TODO: Reconsider file creations
 /// Downloads and transcribes a message.
 ///
 /// Returns transcription as a String
-async fn download_and_transcribe(bot: &AutoSend<Bot>, file_id: &str) -> Result<String, String> {
+async fn download_and_transcribe(
+    bot: &AutoSend<Bot>,
+    file_id: &str,
+    model: &str,
+) -> Result<String, String> {
     let TelegramFile { meta, file_path } = bot
         .get_file(file_id)
         .send()
         .await
         .map_err(|err| format!("Failed to download the file: {err}"))?;
-    let local_filename = meta.file_unique_id.as_str();
 
+    let local_filename = &meta.file_unique_id;
     // Read transcription if already transcribed
-    if let Ok(read) = fs::read_to_string(format!("{local_filename}.txt")).await {
+    if let Ok(read) = fs::read_to_string(format!("{local_filename}.vtt")).await {
         return Ok(read);
     }
 
@@ -54,7 +79,7 @@ async fn download_and_transcribe(bot: &AutoSend<Bot>, file_id: &str) -> Result<S
 
     println!("Finished downloading");
 
-    let transcribed = transcribe_file(local_filename).await;
+    let transcribed = transcribe_file(local_filename, model).await;
 
     // errors if file was removed or permissions changed
     fs::remove_file(local_filename)
@@ -65,16 +90,16 @@ async fn download_and_transcribe(bot: &AutoSend<Bot>, file_id: &str) -> Result<S
     transcribed
 }
 
-async fn transcribe_file(path: &str) -> Result<String, String> {
+async fn transcribe_file(path: &str, model: &str) -> Result<String, String> {
     if cfg!(target_os = "windows") {
         panic!()
     }
 
-    eprintln!("Starting transcription of {}.", path);
+    println!("Starting transcription of {}.", path);
     let output = OsCommand::new("sh")
         .arg("-c")
         .arg(format!(
-            "whisper --model small --language pl --task transcribe {path}"
+            "whisper --model {model} --language pl --task transcribe {path}"
         ))
         .output();
 
@@ -82,4 +107,8 @@ async fn transcribe_file(path: &str) -> Result<String, String> {
         Ok(result) => Ok(String::from_utf8_lossy(&result.stdout).to_string()),
         Err(e) => Err(format!("Failed to execute the transcriber: {e}")),
     }
+}
+
+pub fn is_valid_model(model: &str) -> bool {
+    AVAILABLE_MODELS.contains(model)
 }
